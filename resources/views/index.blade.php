51f660c32818a646760f2a5f59ea9d08e7f1ef1a
Alexa <lori.w@live.cn>
2021-11-21 15:30:43 +0800

    init

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -36,74 +36,74 @@
     <div id="container">
         <div class="section active" data-anchor="home">
             <div class="slide">
-                <div class="page" style="background-image: url({{asset('/images/pages/1-1.png')}})"></div>
+                <div class="page" style="background-image: url({{asset('/images/pages/1-1.jpg')}})"></div>
             </div>
             <div class="slide">
-                <div class="page" style="background-image: url({{asset('/images/pages/1-2.png')}})"></div>
+                <div class="page" style="background-image: url({{asset('/images/pages/1-2.jpg')}})"></div>
             </div>
             <div class="slide">
-                <div class="page" style="background-image: url({{asset('/images/pages/1-3.png')}})"></div>
+                <div class="page" style="background-image: url({{asset('/images/pages/1-3.jpg')}})"></div>
             </div>
             <div class="slide">
-                <div class="page" style="background-image: url({{asset('/images/pages/1-4.png')}})"></div>
+                <div class="page" style="background-image: url({{asset('/images/pages/1-4.jpg')}})"></div>
             </div>
             <div class="slide">
-                <div class="page" style="background-image: url({{asset('/images/pages/1-5.png')}})"></div>
+                <div class="page" style="background-image: url({{asset('/images/pages/1-5.jpg')}})"></div>
             </div>
         </div>
         <div class="section" data-anchor="aboutcitta1">
             <div class="slide">
-                <div class="page" style="background-image: url({{asset('/images/pages/2-1.png')}})"></div>
+                <div class="page" style="background-image: url({{asset('/images/pages/2-1.jpg')}})"></div>
             </div>
             <div class="slide">
-                <div class="page" style="background-image: url({{asset('/images/pages/2-2.png')}})"></div>
+                <div class="page" style="background-image: url({{asset('/images/pages/2-2.jpg')}})"></div>
             </div>
             <div class="slide">
-                <div class="page" style="background-image: url({{asset('/images/pages/2-3.png')}})"></div>
+                <div class="page" style="background-image: url({{asset('/images/pages/2-3.jpg')}})"></div>
             </div>
             <div class="slide">
-                <div class="page" style="background-image: url({{asset('/images/pages/2-4.png')}})"></div>
+                <div class="page" style="background-image: url({{asset('/images/pages/2-4.jpg')}})"></div>
             </div>
             <div class="slide">
-                <div class="page" style="background-image: url({{asset('/images/pages/2-5.png')}})"></div>
+                <div class="page" style="background-image: url({{asset('/images/pages/2-5.jpg')}})"></div>
             </div>
             <div class="slide">
-                <div class="page" style="background-image: url({{asset('/images/pages/2-6.png')}})"></div>
+                <div class="page" style="background-image: url({{asset('/images/pages/2-6.jpg')}})"></div>
             </div>
         </div>
         <div class="section" data-anchor="aboutcitta2">
             <div class="slide">
-                <div class="page" style="background-image: url({{asset('/images/pages/3-1.png')}})"></div>
+                <div class="page" style="background-image: url({{asset('/images/pages/3-1.jpg')}})"></div>
             </div>
             <div class="slide">
-                <div class="page" style="background-image: url({{asset('/images/pages/3-2.png')}})"></div>
+                <div class="page" style="background-image: url({{asset('/images/pages/3-2.jpg')}})"></div>
             </div>
         </div>
         <div class="section" data-anchor="aboutkwah1">
             <div class="slide">
-                <div class="page" style="background-image: url({{asset('/images/pages/4-1.png')}})"></div>
+                <div class="page" style="background-image: url({{asset('/images/pages/4-1.jpg')}})"></div>
             </div>
             <div class="slide">
-                <div class="page" style="background-image: url({{asset('/images/pages/4-2.png')}})"></div>
+                <div class="page" style="background-image: url({{asset('/images/pages/4-2.jpg')}})"></div>
             </div>
             <div class="slide">
-                <div class="page" style="background-image: url({{asset('/images/pages/4-3.png')}})"></div>
+                <div class="page" style="background-image: url({{asset('/images/pages/4-3.jpg')}})"></div>
             </div>
         </div>
         <div class="section" data-anchor="aboutkwah2">
-            <div class="page" style="background-image: url({{asset('/images/pages/5-1.png')}})"></div>
+            <div class="page" style="background-image: url({{asset('/images/pages/5-1.jpg')}})"></div>
         </div>
         <div class="section" data-anchor="aboutkwah3">
-            <div class="page" style="background-image: url({{asset('/images/pages/5-2.png')}})"></div>
+            <div class="page" style="background-image: url({{asset('/images/pages/5-2.jpg')}})"></div>
         </div>
         <div class="section" data-anchor="aboutkwah4">
-            <div class="page" style="background-image: url({{asset('/images/pages/5-3.png')}})"></div>
+            <div class="page" style="background-image: url({{asset('/images/pages/5-3.jpg')}})"></div>
         </div>
         <div class="section" data-anchor="aboutkwah5">
-            <div class="page" style="background-image: url({{asset('/images/pages/5-4.png')}})"></div>
+            <div class="page" style="background-image: url({{asset('/images/pages/5-4.jpg')}})"></div>
         </div>
         <div class="section" data-anchor="contactus">
-            <div class="page" style="background-image: url({{asset('/images/pages/6-1.png')}})"></div>
+            <div class="page" style="background-image: url({{asset('/images/pages/6-1.jpg')}})"></div>
         </div>
     </div>
     <div id="header">
@@ -124,35 +124,38 @@
             </div>
         </div>
         <div class="menus">
-        <ul id="menus">
-            <li data-menuanchor="home" class="active"><a href="#home">{{__("首页")}}</a></li>
-            <li><a href="#">{{__("关于臻逸")}}</a>
-                <ul>
-                    <li data-menuanchor="aboutcitta1"><a href="#aboutcitta1">{{__("品牌概念")}}</a></li>
-                    <li data-menuanchor="aboutcitta2"><a href="#aboutcitta2">{{__("品牌创建")}}</a></li>
-                </ul>
-            </li>
-            <li><a href="#">{{__("关于嘉华")}}</a>
-                <ul>
-                    <li data-menuanchor="aboutkwah1"><a href="#aboutkwah1">{{__("核心价值")}}</a></li>
-                    <li data-menuanchor="aboutkwah2"><a href="#aboutkwah2">{{__("品牌愿景")}}</a></li>
-                    <li data-menuanchor="aboutkwah3"><a href="#aboutkwah3">{{__("品牌定位")}}</a></li>
-                    <li data-menuanchor="aboutkwah4"><a href="#aboutkwah4">{{__("物业服务")}}</a></li>
-                    <li data-menuanchor="aboutkwah5"><a href="#aboutkwah5">{{__("关于我们")}}</a></li>
-                </ul>
-            </li>
-            <li data-menuanchor="contactus"><a href="#contactus">{{__("联系我们")}}</a></li>
-        </ul>
-    </div>
-    <div class="footer">
-        <a href="" download=""><img src="{{asset('images/icon-download.png')}}" alt="download" /></a>
-    </div>
+            <ul id="menus">
+                <li data-menuanchor="home" class="active"><a href="#home">{{__("首页")}}</a></li>
+                <li><a href="#">{{__("关于臻逸")}}</a>
+                    <ul>
+                        <li data-menuanchor="aboutcitta1"><a href="#aboutcitta1">{{__("品牌概念")}}</a></li>
+                        <li data-menuanchor="aboutcitta2"><a href="#aboutcitta2">{{__("品牌创建")}}</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">{{__("关于嘉华")}}</a>
+                    <ul>
+                        <li data-menuanchor="aboutkwah1"><a href="#aboutkwah1">{{__("核心价值")}}</a></li>
+                        <li data-menuanchor="aboutkwah2"><a href="#aboutkwah2">{{__("品牌愿景")}}</a></li>
+                        <li data-menuanchor="aboutkwah3"><a href="#aboutkwah3">{{__("品牌定位")}}</a></li>
+                        <li data-menuanchor="aboutkwah4"><a href="#aboutkwah4">{{__("物业服务")}}</a></li>
+                        <li data-menuanchor="aboutkwah5"><a href="#aboutkwah5">{{__("关于我们")}}</a></li>
+                    </ul>
+                </li>
+                <li data-menuanchor="contactus"><a href="#contactus">{{__("联系我们")}}</a></li>
+            </ul>
+        </div>
+        <div class="footer">
+            <a href="" download=""><img src="{{asset('images/icon-download.png')}}" alt="download" /></a>
+        </div>
     </div>
     <div id="menuWrapper" class="hidden"></div>
     <div class="arrowBottom">
         <img src="{{asset('images/icon-arrow-down.png')}}" alt="next" />
     </div>
     <script>
+        window.oncontextmenu = function(e) {
+            e.preventDefault();
+        }
         function resizePage() {
             const height = $(".fp-tableCell").height();
             const width = $(".fp-tableCell").width();
@@ -178,13 +181,13 @@
                 right: left + "px"
             });
             $(".fp-controlArrow.fp-next").css({
-                right: (left+140)+"px"
+                right: (left + 140) + "px"
             });
             $(".fp-controlArrow.fp-prev").css({
-                left: (left+140)+"px"
+                left: (left + 140) + "px"
             });
             $(".arrowBottom").css({
-                left: (left+140)+"px"
+                left: (left + 140) + "px"
             });
         }
         $().ready(function() {
