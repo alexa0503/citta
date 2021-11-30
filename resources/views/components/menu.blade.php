ec06cf7e36869eb1f9df6e4a0f0ecb03ea17e3f1
Alexa <lori.w@live.cn>
2021-11-30 18:41:51 +0800

    调整

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -2,7 +2,7 @@
     <div class="header">
         <div class="languages">
             @foreach(config('app.languages') as $key => $value)
-            <a href="{{url()->current()}}?lang={{$key}}" {{App::currentLocale() === $key?'class=active':''}}>{{$value}}</a>
+            <a href="/?lang={{$key}}" {{App::currentLocale() === $key?'class=active':''}}>{{$value}}</a>
             @endforeach
         </div>
         <div class="close">
@@ -13,12 +13,10 @@
         <ul id="menus">
             <li data-menuanchor="home"><a href="{{ url('/?lang='.App::currentLocale()) }}#home">{{__("首页")}}</a></li>
             <li data-menuanchor="aboutcitta1"><a href="{{ url('/?lang='.App::currentLocale()) }}#aboutcitta1">{{__("关于臻逸")}}</a></li>
-            <li data-menuanchor="projects" {{isset($page) && $page === 'projects'?' class="active"':''}}><a href="{{ url('/prjects?lang='.App::currentLocale()) }}">{{__("精彩项目")}}</a>
-                @if(isset($page) && $page === 'projects')
+            <li data-menuanchor="projects" {{isset($page) && $page === 'projects'?' class=active':''}}><a href="javascript:;">{{__("精彩项目")}}</a>
                 <ul>
-                    <li data-menuanchor=""><a href="#">{{__("臻逸恒丰精选式公寓")}}</a></li>
+                    <li data-menuanchor=""><a href="{{ url('/prjects?lang='.App::currentLocale()) }}">{{__("臻逸恒丰精选式公寓")}}</a></li>
                 </ul>
-                @endif
             </li>
             <li data-menuanchor="contactus"><a href="{{ url('/?lang='.App::currentLocale()) }}#contactus">{{__("联系我们")}}</a></li>
             <li data-menuanchor="aboutkwah5"><a href="{{ url('/?lang='.App::currentLocale()) }}#aboutkwah5">{{__("关于嘉华")}}</a></li>
